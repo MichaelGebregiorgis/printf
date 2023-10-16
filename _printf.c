21d42129106f46595372e82a0aab452b93e6fd2e
Michael Gebregiorgis <you@example.com>
2023-10-16 03:31:24 -0700

    finally working

--- _printf.c
+++ _printf.c
@@ -15,40 +15,33 @@ int _printf(const char *format, ...)
 {
 	unsigned int count_char = 0;
 	va_list lst_args;
-	
+
 	va_start(lst_args, format);
 
-	for (;*format != '\0'; format++)
+	for (; *format != '\0'; format++)
 	{
 		if (*format == '%')
 		{
 			format += 1;
 			if (*format == 'c')
-			{
 				_putchar(va_arg(lst_args, int));
 				count_char++;
-			}
 			else if (*format == 's')
 			{
 				const char *str = va_arg(lst_args, const char *);
+
 				while (*str)
-				{
 					_putchar(*str);
 					str++;
 					count_char++;
-				}
 			}
 			else if (*format == '%')
-			{
 				_putchar('%');
 				count_char++;
-			}
 			else
-			{
 				_putchar('%');
 				_putchar(*format);
 				count_char += 2;
-			}
 		}
 		else
 		{
