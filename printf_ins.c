f26a49ec5640969ce0a12e49511e169abd79854a
Michael Gebregiorgis <you@example.com>
2023-10-17 08:49:04 -0700

    Print int

--- printf_ins.c
+++ printf_ins.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <limits.h>
-
+#include "main.h"
 
 /**
  * print_int - prints int
@@ -11,6 +11,8 @@
 
 void print_int(int num)
 {
+	int count = 0;
+	int rev = 0;
 	if (num == 0)
 	{
 		_putchar('0');
@@ -21,9 +23,6 @@ void print_int(int num)
 		_putchar('-');
 		num = -num;
 	}
-	int count = 0;
-	int rev = 0;
-
 	while (num > 0)
 	{
 		rev = rev * 10 + num % 10;
@@ -35,47 +34,4 @@ void print_int(int num)
 		_putchar('0' + rev % 10);
 		rev /= 10;
 	}
-}
-
-/**
- * _printf - Handle d and i specifier
- * @format: Datat type
- * Return: number
- */
-int _printf(const char *format, ...)
-{
-	va_list lst_args;
-
-	va_start(lst_args, format);
-	int count = 0;
-
-	for (; *format; format++)
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(lst_args, int);
-
-				print_int(num);
-				count++;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(*format);
-				count += 2;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-	}
-
-	va_end(lst_args);
-	return (count);
 }
